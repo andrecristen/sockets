a9d24e629c03ae68f77cd9b6ee0f82b155d2e1b2
lucas <lucas@singular.inf.br>
2022-09-07 18:19:44 -0300

    inicio client

--- client/src/main/java/Start.java
+++ client/src/main/java/Start.java
@@ -1,6 +1,15 @@
-public class Start {
 
-    public static void main(String[] args) {
-        
+import models.Client;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+public class Start {
+    public static void main(String[] args) throws IOException {
+        Scanner s = new Scanner(System.in);
+        String host = s.next();
+        int port = s.nextInt();
+        new Client(host, port,s);
     }
 }
